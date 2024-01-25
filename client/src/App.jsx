6f9ee309c7bd0836bcbde37a8261710598282e5e
saadkarim402 <saadkarim782@gmail.com>
2024-01-25 19:33:25 +0530

    Header

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Dashboard from './pages/Dashboard'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Header from './Components/Header'
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/sign-up' element={<SignUp/>} />
+      <Route path='/sign-in' element={<SignIn/>} />
+      <Route path='/dashboard' element={<Dashboard/>} />
+      <Route path='/projects' element={<Projects/>} />
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App

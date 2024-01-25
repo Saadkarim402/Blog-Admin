923dbfad3fa6f59e6f305ad7f60f289185a7d877
saadkarim402 <saadkarim782@gmail.com>
2024-01-25 21:54:50 +0530

    Server

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -14,7 +14,7 @@ function Header() {
         className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
         <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-        AnimeVerseHub
+        AnimeVerse
         </span>
       </Link>
       <form >
@@ -33,7 +33,7 @@ function Header() {
             <FaMoon/>
         </Button>
         <Link to='sign-in'>
-            <Button gradientDuoTone='purpleToBlue'>
+            <Button gradientDuoTone='purpleToBlue' outline>
                 Sign-In
             </Button>
         </Link>

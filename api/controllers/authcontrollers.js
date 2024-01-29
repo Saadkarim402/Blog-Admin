90de4d9e97fc5d3dc670115b99dfca740caa5917
saadkarim402 <saadkarim782@gmail.com>
2024-01-29 14:29:05 +0530

    Signin api+JWT

--- api/controllers/authcontrollers.js
+++ api/controllers/authcontrollers.js
@@ -1,8 +1,11 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export const signup = async (req, res,next) => {
+//sign up api
+
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   if (
     !username ||
@@ -12,7 +15,7 @@ export const signup = async (req, res,next) => {
     email === "" ||
     password === ""
   ) {
-    next(errorHandler(400,'All feilds are required'));
+    next(errorHandler(400, "All feilds are required"));
   }
 
   //hashing pass to make it secure
@@ -30,6 +33,40 @@ export const signup = async (req, res,next) => {
     res.json("User data saved in database");
   } catch (error) {
     //next function is used to pass control to the next middleware function in the middleware stack.
+    next(error);
+  }
+};
+
+//signin api
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All feilds are required"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(400, "User not found"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid password"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    
+    //removing password data
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
     next(error);
   }
 };

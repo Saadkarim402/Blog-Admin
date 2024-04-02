1cee1edcd2e2aaf22ebeea8d2d4859ebe4fb2e01
saadkarim402 <saadkarim782@gmail.com>
2024-04-02 12:33:41 +0530

    Added Delete User functionality to Admin

--- api/controllers/user.controllers.js
+++ api/controllers/user.controllers.js
@@ -1,9 +1,11 @@
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 import User from '../models/user.model.js';
+
 export const test = (req, res) => {
-  res.json({ Message: "testing" });
+  res.json({ message: 'API is working!' });
 };
+
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId) {
     return next(errorHandler(403, 'You are not allowed to update this user'));
@@ -49,10 +51,11 @@ export const updateUser = async (req, res, next) => {
     res.status(200).json(rest);
   } catch (error) {
     next(error);
+  }
 };
-}
+
 export const deleteUser = async (req, res, next) => {
-  if (req.user.id !== req.params.userId) {
+  if (!req.user.isAdmin && req.user.id !== req.params.userId) {
     return next(errorHandler(403, 'You are not allowed to delete this user'));
   }
   try {
@@ -62,6 +65,7 @@ export const deleteUser = async (req, res, next) => {
     next(error);
   }
 };
+
 export const signout = (req, res, next) => {
   try {
     res

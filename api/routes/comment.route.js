b7f3da678758806042db25fc4de73e4d81ecc6d1
saadkarim402 <saadkarim782@gmail.com>
2024-04-03 14:19:35 +0530

    Add deleteComment route and functionality

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,9 +1,11 @@
+
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
 import {
   createComment,
-  getPostComments,
+  deleteComment,
   editComment,
+  getPostComments,
   likeComment,
 } from '../controllers/comment.controller.js';
 
@@ -13,5 +15,6 @@ router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
 router.put('/editComment/:commentId', verifyToken, editComment);
+router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
 
 export default router;

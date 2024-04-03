fe46f2f4a45e5ceee6dc43f58abcc14a2c6008c6
saadkarim402 <saadkarim782@gmail.com>
2024-04-03 18:55:47 +0530

    Add search functionality to header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
 import PostPage from './pages/PostPage';
 import ScrollToTop from './components/ScrollToTop';
+import Search from './pages/Search';
 
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
       <Route path='/about' element={<About/>} />
       <Route path='/sign-up' element={<SignUp/>} />
       <Route path='/sign-in' element={<SignIn/>} />
+      <Route path='/search' element={<Search />} />
       {/* to make dashboard private we used private route which only allows u to enter into a page if authenticated*/}
       <Route element={<PrivateRoute />}>
           <Route path='/dashboard' element={<Dashboard />} />

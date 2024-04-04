77f3b02ab3a85b3b2b75af8df2a500113b21498f
saadkarim402 <saadkarim782@gmail.com>
2024-04-05 00:00:48 +0530

    Update API and client files

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -18,7 +18,7 @@ export default function FooterCom() {
               className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
             >
               <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-                AnimeVerse
+                ByteBlogs
               </span>
             </Link>
           </div>
@@ -26,19 +26,19 @@ export default function FooterCom() {
             <div>
               <Footer.Title title="About" />
               <Footer.LinkGroup col>
-                <Footer.Link
+                {/* <Footer.Link
                   href="https://www.100jsprojects.com"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
                   100 JS Projects
-                </Footer.Link>
+                </Footer.Link> */}
                 <Footer.Link
                   href="/about"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  AnimeVerse
+                  ByteBlogs
                 </Footer.Link>
               </Footer.LinkGroup>
             </div>
@@ -46,7 +46,7 @@ export default function FooterCom() {
               <Footer.Title title="Follow us" />
               <Footer.LinkGroup col>
                 <Footer.Link
-                  href="https://www.github.com/sahandghavidel"
+                  href="https://github.com/Saadkarim402"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
@@ -76,7 +76,7 @@ export default function FooterCom() {
             <Footer.Icon href="#" icon={BsInstagram} />
             <Footer.Icon href="#" icon={BsTwitter} />
             <Footer.Icon
-              href="https://github.com/sahandghavidel"
+              href="https://github.com/Saadkarim402"
               icon={BsGithub}
             />
             <Footer.Icon href="#" icon={BsDribbble} />

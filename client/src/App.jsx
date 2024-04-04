3b59d9c0c210e09860652b3c0da5a064400eebcf
saadkarim402 <saadkarim782@gmail.com>
2024-04-05 00:05:44 +0530

    Fix import case in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ import OnlyAdminPrivateRoute from './Components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
 import PostPage from './pages/PostPage';
-import ScrollToTop from './components/ScrollToTop';
+import ScrollToTop from './Components/ScrollToTop';
 import Search from './pages/Search';
 
 
